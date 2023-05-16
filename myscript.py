5e088aeec1e4b6ca8afe85984a744604021a1e12
Kareem Zidane <kareemzidane2@gmail.com>
2023-05-16 10:16:38 +0200

    Update myscript.py

--- myscript.py
+++ myscript.py
@@ -3,4 +3,4 @@ from pyspark.dbutils import DBUtils
 dbutils = DBUtils(spark)
 
 dbutils.jobs.taskValues.set('foo', 42)
-dbutils.jobs.taskValues.get('mytask', 'foo')
+print(dbutils.jobs.taskValues.get('mytask', 'foo'))
